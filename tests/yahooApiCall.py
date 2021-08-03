b9f56480333d242eba4bae777c0a0540942b2c5c
camerondeyy <camerontdey@gmail.com>
2021-08-03 17:36:04 -0400

    Added yahoo finance api call

---
+++ tests/yahooApiCall.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import pandas_datareader.data as dr
+import datetime as dt
+
+class yahooApiCall:
+
+    startTime = dt.datetime(2021, 7, 29)
+    endTime = dt.datetime.today()
+    ticker = input("Please enter ticker: ")
+
+    df = dr.DataReader(ticker, 'yahoo', startTime, endTime)
+    
+    print(df)
