d0ee699f9f4db9a2012499a6c265eb02634f207e
Bradley Rose <rose.bradley9@gmail.com>
2021-08-04 15:56:37 -0400

    adjusted widget placement

--- tests/uiTesting.py
+++ tests/uiTesting.py
@@ -12,13 +12,13 @@ temperature = weather.temp
 humidity = weather.humidity
 
 labelWidget1 = Label(root, text="Calendar", fg='white', bg='black')
-labelWidget1.pack(side=TOP, anchor=NW)
+labelWidget1.pack(side=TOP, anchor=W)
 
 weatherText = "WEATHER\n\n Temperature:  {temp} \n Humidity:  {humid}".format(temp = temperature, humid = humidity)
 labelWidget2 = Label(root, text= weatherText, fg='white', bg='black')
-labelWidget2.pack(side=TOP, anchor=NE)
+labelWidget2.pack(side=TOP, anchor=E)
 
 labelWidget3 = Label(root, text="Stonks", fg='white', bg='black')
-labelWidget3.pack(side=BOTTOM, anchor=SW)
+labelWidget3.pack(side=BOTTOM, anchor=W)
 
 root.mainloop()
