905197c4f086b92e2ff136d523a12af0b9372b03
camerondeyy <camerontdey@gmail.com>
2021-08-24 01:11:26 -0400

    Edited yahooApiCall and added stock portion of uiTesting

--- tests/uiTesting.py
+++ tests/uiTesting.py
@@ -23,6 +23,12 @@ labelWidget2.pack(side=TOP, anchor=E)
 #--------------Stonks--------------------
 labelWidget3 = Label(root, text="Stonks", fg='white', bg='black')
 labelWidget3.pack(side=BOTTOM, anchor=W)
+yahoo = yahooApiCall()
+date = yahoo.date
+open = yahoo.openList
+close = yahoo.closeList
+    
+stocks = "STOCKS\n {d}\n\nTicker:  {t1} \t {t2} \t {t3} \nOpen: {o1} \t {o2} \t {o3} \nClose: {c1} \t {c2} \t {c3} \n ".format(t1=tickers[0], t2=tickers[1], t3=tickers[2], d=date, o1=open[0], o2=open[1], o3=open[2], c1=close[0], c2=close[1], c3=close[2])
 
 #--------------News--------------------
 newsText = "NEWS\n 1:   {title1}\n 2:   {title2}\n 3:   {title3}".format(title1 = newsCall.articleTitle1, title2 = newsCall.articleTitle2, title3 = newsCall.articleTitle3)
--- tests/yahooApiCall.py
+++ tests/yahooApiCall.py
@@ -2,16 +2,33 @@
 #pip install pandas
 #pip install pandas-datareader
 
+
 import pandas as pd
 import pandas_datareader.data as dr
 import datetime as dt
+import math
 
 class yahooApiCall:
 
-    startTime = dt.datetime(2021, 7, 29)
-    endTime = dt.datetime.today()
-    ticker = input("Please enter ticker: ")
+    stockList = ['AAPL', 'TSLA', 'BB']
+    openList = []
+    closeList = []
 
-    df = dr.DataReader(ticker, 'yahoo', startTime, endTime)
+    end = dt.date.today()
+    start = end - dt.timedelta(days=1)
+
+    df = dr.DataReader(stockList, 'yahoo', start, end)
+   
+
+    tickers = df.columns.levels[1]
+    date = end
+
+    #works
+    for i in range(len(stockList)):
+        openPrice = df['Open'][stockList[i]]
+        
+        openList.append(round(*openPrice,2))
     
-    print(df)
+    for j in range(len(stockList)):
+        closePrice = df['Close'][stockList[j]]
+        closeList.append(round(*closePrice,2))
